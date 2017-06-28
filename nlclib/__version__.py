f4fb467a5e11475633085d371a739c95c375138d
Chris Palmer <capalmer1013@gmail.com>
2017-06-28 12:13:43 -0400

    version and license

---
+++ nlclib/__version__.py
@@ -0,0 +1,14 @@
+#   _  __ __   _____ __    ____ ___ 
+#  / |/ // /  / ___// /   /  _// _ )
+# /    // /__/ /__ / /__ _/ / / _  |
+#/_/|_//____/\___//____//___//____/ 
+
+
+__title__ = 'nlclib'
+__description__ = 'Not Lynx Command Line Interface Browser'
+__url__ = 'http://github.com/capalmer1013/NLCLIB'
+__version__ = '0.0.0'
+__author__ = 'Chris Palmer'
+__author_email__ = 'capalmer1013@gmail.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 Chris Palmer'
