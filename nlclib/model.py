9ed13d4210f3f1921ee6e406567809b0fba0ba2a
Chris Palmer <capalmer1013@gmail.com>
2017-06-28 15:29:23 -0400

    adding parsing for the model to generate something to be used by the view

--- nlclib/model.py
+++ nlclib/model.py
@@ -1,16 +1,23 @@
 """ Model Module
 """
+from bs4 import BeautifulSoup
+import html5lib
+
+defaultParser = BeautifulSoup
 class Model(object):
     """ model
         This will store the state of the
         web browser
     """
-    def __init__(self):
+    def __init__(self, parser=defaultParser):
         """ need to figure out what the model will have
         """
+        self.parser = parser
         self.html = ""
+        self.tree = None
 
     def setHtml(self, inHtml):
         """ setting internal state using raw html
         """
         self.html = inHtml
+        self.tree = self.parser(inHtml, "html5lib")
